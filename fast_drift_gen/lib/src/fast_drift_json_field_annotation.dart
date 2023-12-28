b56b0d23180d2b83fbc241f74ebcaf698dcd86ca
vladimir idzo <vladimir.idzo@gmail.com>
2023-12-28 09:54:58 +0200

    1.1.0

---
+++ fast_drift_gen/lib/src/fast_drift_json_field_annotation.dart
@@ -0,0 +1,7 @@
+
+import 'package:fast_drift/fast_drift.dart';
+
+/// The internal representation of parameters entered by the library's user.
+class FastDriftJsonConverterFieldAnnotation implements JsonCon {
+  FastDriftJsonConverterFieldAnnotation();
+}
